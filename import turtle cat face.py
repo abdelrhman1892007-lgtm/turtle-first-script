6ac81b7422033a7962623f6c36f1254975a1070f
abdelrhman1892007-lgtm <abdelrhman1892007@gmail.com>
2026-04-13 11:41:02 +0200

    Add exitonclick() for turtle graphics window
    Added exitonclick() to allow closing the turtle graphics window on click.

--- import turtle cat face.py
+++ import turtle cat face.py
@@ -1,25 +1,24 @@
 import turtle
 
-# إعدادات الشاشة
+
 t = turtle.Turtle()
 t.speed(5)
 t.pensize(3)
 turtle.bgcolor("white")
 
-# دالة لتحريك القلم بدون رسم
+ 
 def move_to(x, y):
     t.penup()
     t.goto(x, y)
     t.pendown()
 
-# 1. رسم الرأس (دائرة)
+
 move_to(0, -100)
-t.color("black", "#FFD700") # لون ذهبي للقطة
+t.color("black", "#FFD700") 
 t.begin_fill()
 t.circle(100)
 t.end_fill()
 
-# 2. رسم الأذن اليمنى (مثلث)
 move_to(60, 80)
 t.begin_fill()
 for i in range(3):
@@ -27,7 +26,6 @@ for i in range(3):
     t.left(120)
 t.end_fill()
 
-# 3. رسم الأذن اليسرى (مثلث)
 move_to(-110, 80)
 t.begin_fill()
 for i in range(3):
@@ -35,21 +33,17 @@ for i in range(3):
     t.left(120)
 t.end_fill()
 
-# 4. رسم العينين
-# العين اليمنى
 move_to(40, 30)
 t.color("black", "black")
 t.begin_fill()
 t.circle(10)
 t.end_fill()
 
-# العين اليسرى
 move_to(-40, 30)
 t.begin_fill()
 t.circle(10)
 t.end_fill()
 
-# 5. رسم الأنف (مثلث صغير مقلوب)
 move_to(0, 10)
 t.color("pink")
 t.begin_fill()
@@ -58,14 +52,11 @@ for i in range(3):
     t.right(120)
 t.end_fill()
 
-# 6. رسم الشوارب
 t.color("black")
-# شوارب يمين
 move_to(20, 0)
 t.goto(80, 10)
 move_to(20, 0)
 t.goto(80, -10)
-# شوارب شمال
 move_to(-20, 0)
 t.goto(-80, 10)
 move_to(-20, 0)
